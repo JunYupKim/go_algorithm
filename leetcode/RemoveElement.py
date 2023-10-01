545907b9f625b17dbb025a95b8437e59cc1c5c68
Jun <superman360@daum.net>
2023-10-01 19:54:37 +0900

    leetcode

---
+++ leetcode/RemoveElement.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        n = nums.count(val)
+        for i in range(n):
+            nums.remove(val)
+        return len(nums)
