6d52061b3765bc71ffff707a846cb77cad842d00
Jun <superman360@daum.net>
2024-01-06 09:39:25 +0900

    leetcode : jump

--- leetcode/jump.py
+++ leetcode/jump.py
@@ -6,9 +6,12 @@ class Solution(object):
         """
         reach = 0 
         for idx,jump in enumerate(nums):
+            # check if idx is over the reach 
             if idx > reach:
                 return False
+            # update reach 
             reach = max(reach,idx+jump)
+            # check if reach is on the target
             if reach >= len(nums)-1:
                 return True
         return False 
